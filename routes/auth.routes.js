4680e7a07e007decc6cab8aaabea80abdb188aed
Vinicius Arvelos <vinicius.arvelos@b2list.com>
2022-04-06 15:39:10 -0300

    more base CSS styles for the website

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-const req = require('express/lib/request');
-
 const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
