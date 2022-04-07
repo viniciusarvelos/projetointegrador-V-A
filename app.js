05516925d7522e4f4333ee63b933a6b3a6f85b78
Vinicius Arvelos <vinicius.arvelos@b2list.com>
2022-04-07 19:25:22 -0300

    implementing authentication & user login

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
+const csrf = require('csurf');
+const expressSession = require('express-session')
 
-const db = require("./data/database");
-const authRoutes = require("./routes/auth.routes");
+const createSessionConfig = require('./config/session');
+const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const authRoutes = require('./routes/auth.routes');
+const productsRoutes = require('./routes/products.routes');
+const baseRoutes = require('./routes/base.routes');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
+
+app.use(baseRoutes);
 app.use(authRoutes);
+app.use(productsRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
