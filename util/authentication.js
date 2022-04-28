a1a380e9278e621b58a4a719bbaa5c9b30900b02
Vinicius Arvelos <vinicius.arvelos@b2list.com>
2022-04-28 10:26:11 -0300

    adding logout functionality

--- util/authentication.js
+++ util/authentication.js
@@ -1,8 +1,13 @@
 function createUserSession(req, user, action) {
     req.session.uid = user._id.toString();
     req.session.save(action);
-}
+  }
 
-module.exports = {
-    createUserSession: createUserSession
-};
+  function destroyUserAuthSession(req) {
+    req.session.uid = null;
+  }
+
+  module.exports = {
+    createUserSession: createUserSession,
+    destroyUserAuthSession: destroyUserAuthSession
+  };
